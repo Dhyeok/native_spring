843eb7cf7069c1b80f625e64407ea0ca803d79e6
dong hyeok <test@test.com>
2025-07-30 09:48:41 +0900

    init

---
+++ catalog-srevice/src/main/java/com/polarbookshop/catalogsrevice/CatalogSreviceApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogsrevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogSreviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogSreviceApplication.class, args);
+	}
+
+}
---
+++ catalog-srevice/src/test/java/com/polarbookshop/catalogsrevice/CatalogSreviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogsrevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogSreviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
