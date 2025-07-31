94dca2798caf538fdd97cebfa6b3ab65edcf30cc
dong hyeok <test@test.com>
2025-07-31 16:53:48 +0900

    update

--- catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,14 +1,113 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest // 스프링 부트 애플리케이션을 테스트하기 위한 셋업 제공
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest( // 완전한 스프링 웹 애플리케이션 콘텍스트와 임의의 포트를 듣는 서블릿 컨테이너를 로드함
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class CatalogServiceApplicationTests {
 
-	@Test // 테스트 케이스를 식별
-	void contextLoads() { // 애플리케이션 콘텍스트가 올바르게 로드했느지 검증하기 위해 사용되는 빈 테스트
+	@Autowired
+	private WebTestClient webTestClient; // 테스트를 위해 rest 엔드포인트를 호출할 유틸리티
+
+	@Test
+	void whenGetRequestWithIdThenBookReturned() {
+		var bookIsbn = "1231231230";
+		var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
+		Book expectedBook = webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(bookToCreate)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(book -> assertThat(book).isNotNull())
+				.returnResult().getResponseBody();
+
+		webTestClient
+				.get()
+				.uri("/books/" + bookIsbn)
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody(Book.class).value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+				});
+	}
+
+	@Test
+	void whenPostRequestThenBookCreated() {
+		var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+
+		webTestClient
+				.post() // http 포스트 요청을 보냄
+				.uri("/books") // books 엔드포인트로 요청을 보냄
+				.bodyValue(expectedBook)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+				});
+	}
+
+	@Test
+	void whenPutRequestThenBookUpdated() {
+		var bookIsbn = "1231231232";
+		var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
+		Book createdBook = webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(bookToCreate)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(book -> assertThat(book).isNotNull())
+				.returnResult().getResponseBody();
+		var bookToUpdate = new Book(createdBook.isbn(), createdBook.title(), createdBook.author(), 7.95);
+
+		webTestClient
+				.put()
+				.uri("/books/" + bookIsbn)
+				.bodyValue(bookToUpdate)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(Book.class).value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.price()).isEqualTo(bookToUpdate.price());
+				});
+	}
+
+	@Test
+	void whenDeleteRequestThenBookDeleted() {
+		var bookIsbn = "1231231233";
+		var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(bookToCreate)
+				.exchange()
+				.expectStatus().isCreated();
+
+		webTestClient
+				.delete()
+				.uri("/books/" + bookIsbn)
+				.exchange()
+				.expectStatus().isNoContent();
+
+		webTestClient
+				.get()
+				.uri("/books/" + bookIsbn)
+				.exchange()
+				.expectStatus().isNotFound()
+				.expectBody(String.class).value(errorMessage ->
+						assertThat(errorMessage).isEqualTo("The book with ISBN " + bookIsbn + " was not found.")
+				);
 	}
 
 }
