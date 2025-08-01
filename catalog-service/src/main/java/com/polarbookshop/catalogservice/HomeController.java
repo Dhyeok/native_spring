2c2b44fbd8af367b6ed2ed327d309778b029206d
dong hyeok <test@test.com>
2025-08-01 12:28:13 +0900

    update

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,5 +1,6 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
