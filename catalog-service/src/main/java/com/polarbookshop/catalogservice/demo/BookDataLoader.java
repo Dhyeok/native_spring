172c4584b21bef41e852ca509f5a5fa603d520c0
dong hyeok <test@test.com>
2025-08-11 16:13:00 +0900

    update

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -23,8 +23,8 @@ public class BookDataLoader {
     @EventListener(ApplicationReadyEvent.class) // applicationREadyEvent가 발생하면 테스트 데이터 생성이 시작된다. 이 이벤트는 애블리케이션 시작 단계가 완료되면 발생한다.
     public void loadBookTestData() {
         bookRepository.deleteAll(); // 빈 데이터베이스로 시작하기 위해 기존 책이 있다면 모두 삭제한다.
-        var book1 = new Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
-        var book2 = new Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
+        var book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
+        var book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
         bookRepository.saveAll(List.of(book1, book2));
     }
 }
